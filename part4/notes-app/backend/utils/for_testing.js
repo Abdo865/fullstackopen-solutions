6425075603474050d5d7ac5ef3cee529855482de
Abdo Nasser <abdonasser865@gmail.com>
2023-07-05 23:35:27 +0300

    part 4-4 and 4-3 done

---
+++ part4/notes-app/backend/utils/for_testing.js
@@ -0,0 +1,9 @@
+const reverse = (string) => string.split("").reverse().join("");
+
+const average = (arr) => {
+  const reducer = (sum, item) => sum + item;
+
+  return arr.length === 0 ? 0 : arr.reduce(reducer, 0) / arr.length;
+};
+
+module.exports = { reverse, average };
