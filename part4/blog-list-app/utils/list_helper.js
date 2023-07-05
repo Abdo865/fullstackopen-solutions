3531178a12c08e89abd9bd2d2ef8a4c5cf5ff100
Abdo Nasser <abdonasser865@gmail.com>
2023-07-05 23:49:57 +0300

    part 4-5 done

--- part4/blog-list-app/utils/list_helper.js
+++ part4/blog-list-app/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => (sum += blog.likes), 0);
 };
 
+const favoriteBlog = (blogs) => {
+  let fav = blogs[0];
+  blogs.forEach((blog) => {
+    if (blog["likes"] > fav["likes"]) fav = blog;
+  });
+  return { title: fav["title"], author: fav["author"], likes: fav["likes"] };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
